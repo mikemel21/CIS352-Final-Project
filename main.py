393cde9555a5347bbb6435b21db82d7ff9427a71
mikemel21 <mikemelei905@gmail.com>
2024-04-01 19:51:58 -0400

    normalization interactivity

--- main.py
+++ main.py
@@ -43,20 +43,41 @@ df = df.astype({'age': int})
 
 
 def add_parameter_ui(classifier_name):
-    pass
+    params = dict()
+    if classifier_name == "SVM":
+        C = st.slider("C", 0.01, 10.0)
+        params["C"] = C
+    elif classifier_name == "Random Forest":
+        criterion = st.selectbox(label="Select Criterion", options=["gini", "entropy", "log_loss"])
+        max_depth = st.slider("max depth", 2, 10)
+        n_estimators = st.slider("n_estimators", 2, 50)
+        params["max_depth"] = max_depth
+        params["n_estimators"] = n_estimators
+        params["criterion"] = criterion
+    elif classifier_name == "Decision Tree":
+        max_depth = st.slider("C", 2, 10)
+        randomState = st.slider("Random State", 0, 10) # Added later w/o knowing what Im doing
+        splitter = st.selectbox(label="Select Splitter", options=["best", "random"])
+        criterion = st.selectbox(label="Select Crtiterion", options=["gini", "entropy", "log_loss"])
+        params["max_depth"] = max_depth
+        params["random-state"] = randomState # Added later w/o knowing what Im doing
+        params["splitter"] = splitter
+        params["criterion"] = criterion
+    return params
 
 
-def get_classifier(classifier_name):
+def get_classifier(classifier_name, params):
     match classifier_name:
         case "Random Forest":
-            return RandomForestClassifier()
+            return RandomForestClassifier(criterion=params["criterion"], 
+                                          max_depth=params["max_depth"],
+                                          n_estimators=params["n_estimators"])
         case "AdaBoost":
             return AdaBoostClassifier()
         case "SVM":
             return SVC()
         case "Decision Tree":
             return DecisionTreeClassifier()
-
 
 def get_normalizer(normalization_name):
     match normalization_name:
@@ -91,27 +112,31 @@ st.write(df)
 # 
 # st.write(paramSection)
 
+
 with st.expander("**Select Classifier**"):
     classifier_name = st.selectbox(label="**Classifier options**",
                                            options=["Random Forest", "AdaBoost", "SVM", "Decision Tree"])
 
-    classifier = get_classifier(classifier_name)
-
+    #classifier = get_classifier(classifier_name)
+    
     st.write("Set Classifier Parameters")
 
+    params = add_parameter_ui(classifier_name)
+    classifier = get_classifier(classifier_name, params)
+    
     # this gets the parameters and default values for each one
-    classifierParValues = []
-    for par, defVal in classifier.get_params().items():
-        tempVal = st.text_input(label=f"**{par}**", placeholder=defVal)
+    # classifierParValues = []
+    # for par, defVal in classifier.get_params().items():
+    #     tempVal = st.text_input(label=f"**{par}**", placeholder=defVal)
 
-        # TODO: add description of parameter from docstring here in st.write()
+    #     # TODO: add description of parameter from docstring here in st.write()
 
-        # if nothing has been put in this field use default
-        if tempVal == "":
-            classifierParValues.append(defVal)
+    #     # if nothing has been put in this field use default
+    #     if tempVal == "":
+    #         classifierParValues.append(defVal)
 
-        else:
-            classifierParValues.append(tempVal)
+    #     else:
+    #         classifierParValues.append(tempVal)
 
 
 with st.expander("Select Normalization Technique"):
@@ -140,8 +165,17 @@ with st.expander("Select Normalization Technique"):
     else:
         st.write("No normalization selected")
 
+x = df.iloc[:, :9]
+y = df.iloc[:, 10]
+st.write(y)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=params.get("random-state"))
+pipe = Pipeline([
+    ('scaler', normalizer),
+    ('classifier', classifier)])
 
-# pipe = Pipeline([
-#     ('scaler', scaler),
-#     ('classifier', classifier)])
+pipe.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
+st.write(f'Classifier = {classifier_name}')
+st.write(f"Accuracy = {acc}")
