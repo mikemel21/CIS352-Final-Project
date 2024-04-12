a9009a2e8fe564683433bc913ce180bbf51d79d4
mikemel21 <mikemelei905@gmail.com>
2024-04-12 01:29:46 -0400

    Age distribution visualization

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import inspect
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
@@ -12,7 +14,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import PowerTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.pipeline import Pipeline # For setting up pipeline
+from sklearn.pipeline import Pipeline  # For setting up pipeline
 
 # title
 st.title("CIS 335 Project: Stroke Dataset")
@@ -21,25 +23,40 @@ st.title("CIS 335 Project: Stroke Dataset")
 df = pd.read_csv("stroke-dataset.csv")
 
 # remove ID column
-df = df.drop(['id'], axis=1)
+df = df.drop(["id"], axis=1)
 
 # Replace all N/A values in BMI column with the mean of that column
-df['bmi'] = df['bmi'].fillna(df['bmi'].mean()).round(1)
+df["bmi"] = df["bmi"].fillna(df["bmi"].mean()).round(1)
 
 
 # some data pre processing
-df['gender'] = df['gender'].apply(lambda x: x.lower() if isinstance(x, str) else x)
-df['ever_married'] = df['ever_married'].apply(lambda x: x.lower() if isinstance(x, str) else x)
-df['work_type'] = df['work_type'].apply(lambda x: x.lower() if isinstance(x, str) else x)
-df['Residence_type'] = df['Residence_type'].apply(lambda x: x.lower() if isinstance(x, str) else x)
-df['smoking_status'] = df['smoking_status'].apply(lambda x: x.lower() if isinstance(x, str) else x)
+df["gender"] = df["gender"].apply(lambda x: x.lower() if isinstance(x, str) else x)
+df["ever_married"] = df["ever_married"].apply(
+    lambda x: x.lower() if isinstance(x, str) else x
+)
+df["work_type"] = df["work_type"].apply(
+    lambda x: x.lower() if isinstance(x, str) else x
+)
+df["Residence_type"] = df["Residence_type"].apply(
+    lambda x: x.lower() if isinstance(x, str) else x
+)
+df["smoking_status"] = df["smoking_status"].apply(
+    lambda x: x.lower() if isinstance(x, str) else x
+)
 
-df['gender'].replace(['male', 'female', 'other'], [0, 1, 2], inplace=True)
-df['ever_married'].replace(['yes', 'no'], [0, 1], inplace=True)
-df['work_type'].replace(['children', 'never_work', 'never_worked', 'private', 'self-employed', 'govt_job'], [0, 1, 1, 2, 3, 4], inplace=True)
-df['Residence_type'].replace(['urban', 'rural'], [0, 1], inplace=True)
-df['smoking_status'].replace(['unknown', 'never smoked', 'formerly smoked', 'smokes'], [0, 1, 2, 3], inplace=True)
-df = df.astype({'age': int})
+df["gender"].replace(["male", "female", "other"], [0, 1, 2], inplace=True)
+df["ever_married"].replace(["yes", "no"], [0, 1], inplace=True)
+df["work_type"].replace(
+    ["children", "never_work", "never_worked", "private", "self-employed", "govt_job"],
+    [0, 1, 1, 2, 3, 4],
+    inplace=True,
+)
+df["Residence_type"].replace(["urban", "rural"], [0, 1], inplace=True)
+df["smoking_status"].replace(
+    ["unknown", "never smoked", "formerly smoked", "smokes"], [0, 1, 2, 3], inplace=True
+)
+df = df.astype({"age": int})
+
 
 def add_parameter_ui(classifier_name):
     params = dict()
@@ -49,7 +66,9 @@ def add_parameter_ui(classifier_name):
         params["C"] = C
         params["random-state"] = randomState
     elif classifier_name == "Random Forest":
-        criterion = st.selectbox(label="Select Criterion", options=["gini", "entropy", "log_loss"])
+        criterion = st.selectbox(
+            label="Select Criterion", options=["gini", "entropy", "log_loss"]
+        )
         max_depth = st.slider("max depth", 2, 10)
         n_estimators = st.slider("n_estimators", 2, 50)
         randomState = st.slider("Random State", 1, 10)
@@ -61,7 +80,9 @@ def add_parameter_ui(classifier_name):
         max_depth = st.slider("C", 2, 10)
         randomState = st.slider("Random State", 1, 10)
         splitter = st.selectbox(label="Select Splitter", options=["best", "random"])
-        criterion = st.selectbox(label="Select Crtiterion", options=["gini", "entropy", "log_loss"])
+        criterion = st.selectbox(
+            label="Select Crtiterion", options=["gini", "entropy", "log_loss"]
+        )
         params["max_depth"] = max_depth
         params["random-state"] = randomState
         params["splitter"] = splitter
@@ -74,22 +95,30 @@ def add_parameter_ui(classifier_name):
         params["random-state"] = randomState
     return params
 
+
 def get_classifier(classifier_name, params):
     match classifier_name:
         case "Random Forest":
-            return RandomForestClassifier(criterion=params["criterion"], 
-                                          max_depth=params["max_depth"],
-                                          n_estimators=params["n_estimators"],
-                                          random_state=params["random-state"])
+            return RandomForestClassifier(
+                criterion=params["criterion"],
+                max_depth=params["max_depth"],
+                n_estimators=params["n_estimators"],
+                random_state=params["random-state"],
+            )
         case "AdaBoost":
-            return AdaBoostClassifier(n_estimators=params["n_estimators"], random_state=params["random-state"])
+            return AdaBoostClassifier(
+                n_estimators=params["n_estimators"], random_state=params["random-state"]
+            )
         case "SVM":
             return SVC(C=params["C"], random_state=params["random-state"])
         case "Decision Tree":
-            return DecisionTreeClassifier(max_depth=params["max_depth"],
-                                          splitter=params["splitter"],
-                                          criterion=params["criterion"],
-                                          random_state=params["random-state"])
+            return DecisionTreeClassifier(
+                max_depth=params["max_depth"],
+                splitter=params["splitter"],
+                criterion=params["criterion"],
+                random_state=params["random-state"],
+            )
+
 
 def get_normalizer(normalization_name):
     match normalization_name:
@@ -103,39 +132,40 @@ def get_normalizer(normalization_name):
             return None
 
 
-#st.write(df)
+# st.write(df)
 
 # rd = RandomForestClassifier()
-# 
+#
 # docString = rd.__doc__
-# 
+#
 # docString = docString.split(sep='\n')
-# 
+#
 # for idx, line in enumerate(docString):
 #     if "Parameter" in line:
 #         parameterLine = idx
 #         st.write(line)
-# 
+#
 #     if "Attribute" in line:
 #         attributeLine = idx
 #         st.write(line)
-# 
+#
 # paramSection = docString[parameterLine-1:attributeLine]
-# 
+#
 # st.write(paramSection)
 
-
 with st.expander("**Select Classifier**"):
-    classifier_name = st.selectbox(label="**Classifier options**",
-                                           options=["Random Forest", "AdaBoost", "SVM", "Decision Tree"])
+    classifier_name = st.selectbox(
+        label="**Classifier options**",
+        options=["Random Forest", "AdaBoost", "SVM", "Decision Tree"],
+    )
 
-    #classifier = get_classifier(classifier_name)
-    
+    # classifier = get_classifier(classifier_name)
+
     st.write("Set Classifier Parameters")
 
     params = add_parameter_ui(classifier_name)
     classifier = get_classifier(classifier_name, params)
-    
+
     # this gets the parameters and default values for each one
     # classifierParValues = []
     # for par, defVal in classifier.get_params().items():
@@ -152,8 +182,10 @@ with st.expander("**Select Classifier**"):
 
 
 with st.expander("Select Normalization Technique"):
-    normalization_name = st.selectbox(label="Normalization Options",
-                                              options=["None", "MinMax", "Z-Score", "Power Transform"])
+    normalization_name = st.selectbox(
+        label="Normalization Options",
+        options=["None", "MinMax", "Z-Score", "Power Transform"],
+    )
 
     normalizer = get_normalizer(normalization_name)
 
@@ -178,15 +210,36 @@ with st.expander("Select Normalization Technique"):
 x = df.iloc[:, :9]
 y = df.iloc[:, 10]
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=params.get("random-state"))
-pipe = Pipeline([
-    ('scaler', normalizer),
-    ('classifier', classifier)])
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=params.get("random-state")
+)
+pipe = Pipeline([("scaler", normalizer), ("classifier", classifier)])
 
 pipe.fit(X_train, y_train)
 y_pred = pipe.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 score = pipe.score(X_test, y_test)
 
-st.write(f'Classifier = {classifier_name}')
-st.write(f"Score = ", score)
+st.write(f"Classifier = {classifier_name}")
+st.write("Score = ", score)
+
+# age distribution visualization
+palette = {"No Stroke": "blue", "Stroke": "red"}
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.histplot(
+    data=df,
+    x="age",
+    hue=df["stroke"].replace({0: "No Stroke", 1: "Stroke"}),
+    palette=palette,
+    binwidth=5,
+    kde=False,
+    multiple="dodge",
+    ax=ax,
+)
+ax.set_title("Age Distribution By Stroke")
+age_bins = np.linspace(df["age"].min(), df["age"].max(), 30)
+ax.set_xticks(age_bins)
+ax.set_xticklabels([int(x) for x in age_bins])
+ax.set_xlabel("Age")
+ax.set_ylabel("Count")
+st.pyplot(fig)
